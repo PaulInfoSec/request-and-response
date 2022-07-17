12793042d3e526c16551d384939d87acb0fbb2a2
ITInfraSec <paul_cullum@hotmail.com>
2022-07-17 12:09:34 +0800

    updated script

---
+++ staus_check.py
@@ -0,0 +1,20 @@
+import csv
+import requests
+
+
+# open stored CSV of urls to check and store the response status code in output.csv
+
+
+with open('urls.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    # Iterate through each line of the csv file
+    for row in reader:
+        r = requests.get(row['URL'])
+        stats = r.status_code
+        sites = r.url
+        # print(str(stats) + " " + (sites)) 
+        with open ('output.csv', 'a') as outf:
+            outf.write(str(stats) + "," + (sites) + '\n')
+
+
+
